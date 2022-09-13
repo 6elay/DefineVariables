8a1970360a2c279be2b3a6a638a3111df284117f
elay6 <elaynafl06@gmail.com>
2022-09-13 09:56:23 -0500

    Initial commit

---
+++ src/Variables.java
@@ -0,0 +1,40 @@
+public class Variables
+{
+    public static void main ( String[] args )
+    {
+        //define 1 variable of each of the
+        //following data types
+        //byte	short		int 		long
+        //float	double
+        //char      boolean	String
+
+        //integer variables
+        byte byteOne = 127;
+        short sh0rt = 32767;
+        int tni = 214749364;
+        long l0ng = 354L;
+        float fl0at = 54.34F;
+        double d0ubl3 = 4234.2342;
+        char ch2r = 'A';
+        boolean b00l3an = true;
+        String str1ng = "Hello!";
+
+        System.out.println("/////////////////////////////////");
+        System.out.println("* Elayna Flores           09/13/22*");
+        System.out.println("*                               *");
+        System.out.println("*        integer types          *");
+        System.out.println("*                               *");
+        System.out.println("*8 bit - byteOne = "+byteOne+"\t\t*");
+        System.out.println("*16 bits - sh0rt = "+sh0rt+"\t\t*");
+        System.out.println("*32 bits - tni = " +tni+"\t\t*");
+        System.out.println("64 bits - l0ng = " +l0ng+"\t\t*");
+        System.out.println("32 bits - fl0at = " +fl0at+"\t\t*");
+        System.out.println("64 bits - d0ubl3 = " +d0ubl3+"\t\t*");
+        System.out.println("*16 bits - ch2r = "+ch2r+"\t\t*");
+        System.out.println("*true or false - b00l3an = "+b00l3an+"\t\t*");
+        System.out.println("*words or phrases - str1ng = "+str1ng+"\t\t*");
+
+
+
+    }
+}
